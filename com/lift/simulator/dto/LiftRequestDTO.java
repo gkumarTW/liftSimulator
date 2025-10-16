65db968da8fa12c292ca386a1c71b4694e1b7994
ngaddamanugu <ngaddamanugu02@gmail.com>
2025-10-16 17:32:58 +0530

    Removed the usage of record class which was suggested by SonarQube

--- com/lift/simulator/dto/LiftRequestDTO.java
+++ com/lift/simulator/dto/LiftRequestDTO.java
@@ -3,22 +3,34 @@ package com.lift.simulator.dto;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record LiftRequestDTO(int pickUpFloor, int dropOffFloor, int passengerCount) {
+public class LiftRequestDTO {
+    private final int pickUpFloor;
+    private final int dropOffFloor;
+    private final int passengerCount;
+
     @JsonCreator
-    public LiftRequestDTO(
-            @JsonProperty("fromFloor") int pickUpFloor,
-            @JsonProperty("toFloor") int dropOffFloor,
-            @JsonProperty("noOfPassengers") int passengerCount
-    ) {
+    public LiftRequestDTO(@JsonProperty("fromFloor") int pickUpFloor,
+                          @JsonProperty("toFloor") int dropOffFloor,
+                          @JsonProperty("noOfPassengers") int passengerCount) {
         this.pickUpFloor = pickUpFloor;
         this.dropOffFloor = dropOffFloor;
         this.passengerCount = passengerCount;
     }
 
+    public int pickUpFloor() {
+        return pickUpFloor;
+    }
+
+    public int dropOffFloor() {
+        return dropOffFloor;
+    }
+
+    public int passengerCount() {
+        return passengerCount;
+    }
+
     @Override
     public String toString() {
-        return "pickUpFloor:" + pickUpFloor +
-                " dropOffFloor:" + dropOffFloor +
-                " passengerCount:" + passengerCount;
+        return "pickUpFloor:" + pickUpFloor + " dropOffFloor:" + dropOffFloor + " passengerCount:" + passengerCount;
     }
 }
